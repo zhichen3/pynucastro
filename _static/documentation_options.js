5d3462ea9fbba6896fdf77da261c776147589367
zhichen3 <zhichen3@users.noreply.github.com>
2022-05-30 03:12:58 +0000

    deploy: ba23dc6ecf5195a1f736445e399aca8be7ea8bcd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.0.post99+g209c1e3',
-    LANGUAGE: 'None',
+    VERSION: '1.6.0.post109+gba23dc6',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
+    ENABLE_SEARCH_SHORTCUTS: false,
 };
