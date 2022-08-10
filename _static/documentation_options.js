cd129dfb72188cfc2ec2a80efaf122cdafe9645f
zhichen3 <zhichen3@users.noreply.github.com>
2022-08-10 20:22:32 +0000

    deploy: 6822a7c685d2c6e309cb47a0905c5acb3f82a341

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.0.post159+gf7fc811',
+    VERSION: '1.6.0.post161+g6822a7c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
